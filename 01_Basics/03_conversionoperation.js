7c37bc1cbb7fe979fb933ad62d7ad0b73f958ac5
syedrawoof <syedrawoof115@gmail.com>
2023-10-07 23:47:20 +0530

    operations

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -1,8 +1,10 @@
 let score="11fgh";
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
 let valinNumber=Number(score)
-console.log(typeof valinNumber);
-console.log(valinNumber);
+// console.log(typeof valinNumber);
+// console.log(valinNumber);
+// operations
+console.log("1"+"4")
