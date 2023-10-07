3f95c0601d05f36436fea7e6620148f55a6e8f35
syedrawoof <syedrawoof115@gmail.com>
2023-10-07 18:17:33 +0530

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";// treat all JS code as newer version
+
+// alert(4+4) // we are using nodejs , not browser
+
+/* nnssbbu 
+            Primitive data types
+                                typeof return value
+null        standalone value        object
+number                              number
+string      ""                      string
+symbol      unique                  symbol
+bigint                              bigint
+boolean     true/false              boolean
+undefind    value id not define     undefind
+*/
+let number=115
+let boolean=true
+console.log(typeof(number));
+console.log(typeof(boolean));
