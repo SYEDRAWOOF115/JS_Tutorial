5bc508229109bad07ca449bc9bd946c401a6495d
syedrawoof <syedrawoof115@gmail.com>
2023-10-07 17:50:26 +0530

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID=115;
+let accountEmail="syedrawoof115@gmail.com";
+var accountPassword="1234"
+accountCity="Hyderabad"
+let accountstate
+// accountID=2 //not allowed
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+accountEmail="syed.com"
+accountPassword="123"
+accountCity="Bangalore"
+console.log(accountID);
+console.log(accountEmail,accountPassword,accountCity,accountstate);
