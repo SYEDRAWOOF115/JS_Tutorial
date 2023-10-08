9526497b55f0c70ae601da61afc9f71215fc6751
syedrawoof <syedrawoof115@gmail.com>
2023-10-08 21:08:38 +0530

    stringd

---
+++ 01_Basics/04_strings.js
@@ -0,0 +1,16 @@
+const name="syed"
+const count=115
+//string interpolation
+console.log(`hello my name is ${name} and my count is ${count}`);
+//string initialization
+const game=new String("rawoof")
+console.log(game);
+console.log(game.length);
+console.log(game.charAt(2));
+console.log(game.indexOf('o'));
+console.log(game.toUpperCase());
+console.log(game.toLocaleLowerCase());
+console.log(game[0]);
+console.log(typeof game);
+
+//strings methods mdn referene
